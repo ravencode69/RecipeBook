e31e38eb50906bbdb8afa50db8a543539edda19d
Richa Gupta <86804513+ravencode69@users.noreply.github.com>
2023-12-23 13:43:50 +0530

    C5_seperationOfRecipeItem

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeItemComponent implements OnInit {
 
+  @Input() recipeItem:Recipe;
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { 
   }
+  
 
 }
